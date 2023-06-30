4775fb20c0150e73e489ef9a935afde721001c1d
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-06-30 23:52:00 +0300

    feat: rabbitmq configuration

---
+++ chainhook-subscriber/src/rabbitmq/constants.ts
@@ -0,0 +1,15 @@
+import { Transport } from "@nestjs/microservices";
+
+export const RabbitMQService = 'RABBITMQ_SERVICE';
+
+export const rabbitMQServiceOptions = {
+    name: RabbitMQService,
+    transport: Transport.RMQ,
+    options: {
+        urls: ['amqp://admin:supersecretpassword@localhost:5672'],
+        queue: 'chainhook_queue',
+        queueOptions: {
+            durable: false,
+        },
+    },
+};
