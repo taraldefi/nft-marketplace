edbefde047cce523629ae52fa300a7170ce506ef
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-06-30 23:47:08 +0300

    feat(consumer): cleanup

--- chainhook-consumer/src/rabbtmq/auction.service.ts
+++ chainhook-consumer/src/rabbtmq/auction.service.ts
@@ -7,15 +7,11 @@ export class AuctionPublisherService {
     constructor(
         @Inject(RabbitMQService)
         private client: ClientProxy,
-    ) {}
+    ) {
+        
+    }
 
     async publishMessage(pattern: string, message: string): Promise<void> {
-
-        console.log(`Publishing message: ${message}`);
-        console.log('Publishing message to pattern: ' + pattern);
-
-        await this.client.emit(pattern, message).toPromise().catch(err => {
-            console.error(`Error while emitting message: ${JSON.stringify(err)}`);
-          });;
+        await this.client.emit(pattern, message).toPromise();
     }
 }
