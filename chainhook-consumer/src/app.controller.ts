b436fb8912a3d8c449c81c6f5f8f45847baeb35f
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-06-30 21:04:21 +0300

    chore: had to disable the auth guard because ofc chainhook doesn't pass the header

--- chainhook-consumer/src/app.controller.ts
+++ chainhook-consumer/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiKeyAuthGuard } from './auth/guard/apikey-auth.guard';
 
-@UseGuards(ApiKeyAuthGuard)
+// @UseGuards(ApiKeyAuthGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -11,5 +11,6 @@ export class AppController {
   @Post('chainhook')
   chainhook(@Body() body: any): void {
     console.log(JSON.stringify(body));
+    console.log('--------------------------------------------')
   }
 }
