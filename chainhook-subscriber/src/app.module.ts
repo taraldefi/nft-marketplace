518f9166eaea9d4c5b74a404dbdb8d7208c0ff94
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-07-01 10:22:12 +0300

    feat(subscriber): initial setup

--- chainhook-subscriber/src/app.module.ts
+++ chainhook-subscriber/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        appConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- chainhook-subscriber/src/main.ts
+++ chainhook-subscriber/src/main.ts
@@ -1,8 +1,54 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeTransactionalContext } from './common/transaction/common';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import validationOptions from './common/validation/validation.options';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  require('tsconfig-paths/register');
+
+  initializeTransactionalContext(); // Initialize cls-hooked
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+
+  app.enableShutdownHooks();
+  app.setGlobalPrefix(configService.get('app.apiPrefix'), {
+    exclude: ['/'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  useContainer(app.select(AppModule), {
+    fallbackOnErrors: true
+  });
+
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+
+  app.use(cookieParser());
+
+  const options = new DocumentBuilder()
+    .setTitle('Taral API')
+    .setDescription('Taral API docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  if (process.env.NODE_ENV === 'development') {
+    fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+  }
+
+  SwaggerModule.setup('docs', app, document);
+
+  const port = configService.get('app.port');
+
+  await app.listen(port);
+  console.log(`Application listening in port: ${port}`);
 }
 bootstrap();
