7b848b8421abee489f01879b9431a1e634e9648f
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-07-03 22:38:38 +0300

    feat: auction module

---
+++ chainhook-subscriber/src/modules/auctions/auction.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuctionEntity } from './entities/auction.entity';
+import { AuctionBidEntity } from './entities/auction.bid.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+        AuctionEntity, AuctionBidEntity
+    ]),
+  ],
+  controllers: [],
+  providers: [
+    ConfigModule,
+    ConfigService
+  ],
+})
+export class AuctionModule {}
