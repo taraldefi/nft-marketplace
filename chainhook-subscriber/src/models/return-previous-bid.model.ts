d3f3f8157640cd932247ff770c5ff5eb6dc4b6d4
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-07-14 17:57:12 +0300

    feat: return previous bid model

---
+++ chainhook-subscriber/src/models/return-previous-bid.model.ts
@@ -0,0 +1,25 @@
+interface ReturnPreviousBid {
+    action: Action;
+    'auction-id': Action;
+    bid: Action;
+    'end-block': Action;
+    'highest-bid': Action;
+    'highest-bidder': Highestbidder;
+    maker: Action;
+    'nft-asset-contract': Action;
+    'previous-bidder': Action;
+    'reserve-price': Action;
+    'start-bid': Action;
+    'start-block': Action;
+    'token-id': Action;
+  }
+  
+  interface Highestbidder {
+    type: string;
+    value: Action;
+  }
+  
+  interface Action {
+    type: string;
+    value: string;
+  }
