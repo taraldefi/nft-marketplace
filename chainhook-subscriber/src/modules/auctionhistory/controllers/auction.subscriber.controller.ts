c35e418c09cbee306a7dbedcf606bec81b170879
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-07-14 18:13:32 +0300

    feat: log received events

--- chainhook-subscriber/src/modules/auctionhistory/controllers/auction.subscriber.controller.ts
+++ chainhook-subscriber/src/modules/auctionhistory/controllers/auction.subscriber.controller.ts
@@ -7,10 +7,26 @@ export class AuctionSubscriberController {
     async handleMessage(@Payload() data: any, @Ctx() context: RmqContext) {
         const channel = context.getChannelRef();
         const originalMsg = context.getMessage();
-        
-        console.log(`Received message: ${JSON.stringify(data)}`);
 
-        // Processing of message...
+        let type = data.type.value as string;
+
+        switch (type) {
+            case 'place-bid':
+                console.log('Place bid event received');
+                break;
+            case 'start-auction':
+                console.log('Start Auction event received');
+                break;
+            case 'set-whitelisted':
+                console.log('Whitelist event received');
+                break;
+            case 'cancel-auction': 
+                console.log('Cancel Auction event received');
+                break;
+            default:
+                console.log('Unknown event received');
+                break;
+        }
 
         // Acknowledge the message
         channel.ack(originalMsg);
