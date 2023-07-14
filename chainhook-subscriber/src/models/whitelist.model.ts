d3b617e953531bb7ac43cdbddd52b144a8283aac
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-07-14 17:48:49 +0300

    feat: add whitelist model

---
+++ chainhook-subscriber/src/models/whitelist.model.ts
@@ -0,0 +1,16 @@
+export interface Whitelist {
+    action:           Action;
+    "asset-contract": Action;
+    type:             Action;
+    whitelisted:      Whitelisted;
+}
+
+export interface Action {
+    type:  string;
+    value: string;
+}
+
+export interface Whitelisted {
+    type:  string;
+    value: boolean;
+}
