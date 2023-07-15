59eb70d3cd4bc33b8b58e76c54dfd549a4309976
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-07-15 13:20:25 +0300

    feat(subscriber): cleanup rabbit service

--- chainhook-subscriber/src/modules/auctionhistory/controllers/auction.subscriber.controller.ts
+++ chainhook-subscriber/src/modules/auctionhistory/controllers/auction.subscriber.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, OnModuleInit } from '@nestjs/common';
-import { EventPattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
 import { RabbitMQHealthService } from '../services/rabbitmq.health.service';
 import { RabbitmqService } from '../services/rabbitmq.service';
 
@@ -14,37 +13,5 @@ export class AuctionSubscriberController implements OnModuleInit {
     onModuleInit() {
         console.log(`The auction subscriber module has been initialized.`);
         this.healthService.checkClientStatus();
-    }
-
-    @EventPattern('auction_event')
-    async handleMessage(@Payload() data: any, @Ctx() context: RmqContext) {
-        console.log('In handle message');
-        const channel = context.getChannelRef();
-        const originalMsg = context.getMessage();
-
-        console.log('Received event: ', data);
-
-        let type = data.type.value as string;
-
-        switch (type) {
-            case 'place-bid':
-                console.log('Place bid event received');
-                break;
-            case 'start-auction':
-                console.log('Start Auction event received');
-                break;
-            case 'set-whitelisted':
-                console.log('Whitelist event received');
-                break;
-            case 'cancel-auction': 
-                console.log('Cancel Auction event received');
-                break;
-            default:
-                console.log('Unknown event received');
-                break;
-        }
-
-        // Acknowledge the message
-        channel.ack(originalMsg);
     }
 }
--- chainhook-subscriber/src/modules/auctionhistory/services/rabbitmq.service.ts
+++ chainhook-subscriber/src/modules/auctionhistory/services/rabbitmq.service.ts
@@ -5,142 +5,172 @@ import { Counter, Registry } from 'prom-client';
 
 @Injectable()
 export class RabbitmqService implements OnModuleInit, OnApplicationShutdown {
-  private queueName = 'chainhook_queue';
-  private connection: amqp.Connection | null = null;
-  private channel: amqp.Channel | null = null;
-  private consuming = false;
+    private queueName = 'chainhook_queue';
+    private connection: amqp.Connection | null = null;
+    private channel: amqp.Channel | null = null;
+    private consuming = false;
 
-  private registry = new Registry();
-  private successfulConnectionCounter = new Counter({
-    name: 'rabbitmq_successful_connections_total',
-    help: 'Total number of successful connections to RabbitMQ',
-    registers: [this.registry],
-  });
-  private connectionErrorCounter = new Counter({
-    name: 'rabbitmq_connection_errors_total',
-    help: 'Total number of connection errors to RabbitMQ',
-    registers: [this.registry],
-  });
+    private registry = new Registry();
+    private successfulConnectionCounter = new Counter({
+        name: 'rabbitmq_successful_connections_total',
+        help: 'Total number of successful connections to RabbitMQ',
+        registers: [this.registry],
+    });
+    private connectionErrorCounter = new Counter({
+        name: 'rabbitmq_connection_errors_total',
+        help: 'Total number of connection errors to RabbitMQ',
+        registers: [this.registry],
+    });
 
-  private logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-    transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    ],
-  });
+    private logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        transports: [
+            new winston.transports.Console(),
+            new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        ],
+    });
 
-  async onModuleInit() {
-    await this.retryConnect();
-    await this.startListening();
-  }
-
-  async onApplicationShutdown() {
-    await this.stopListening();
-    await this.closeConnection();
-  }
-
-  private async connect() {
-    try {
-      this.connection = await amqp.connect('amqp://admin:supersecretpassword@localhost:5673');
-      this.connection.on('error', (err) => {
-        this.logger.error(`Connection error: ${err}`);
-        this.connection = null;
-        this.connectionErrorCounter.inc();
-      });
-      this.connection.on('close', () => {
-        this.logger.info(`Connection closed`);
-        this.connection = null;
-      });
-      this.successfulConnectionCounter.inc();
-    } catch (err) {
-      this.logger.error(`Failed to connect: ${err}`);
-      this.connectionErrorCounter.inc();
-    }
-  }
-
-  private async createChannel() {
-    if (!this.connection) {
-      this.logger.error(`Cannot create a channel without a connection`);
-      return;
+    async onModuleInit() {
+        await this.retryConnect();
+        await this.startListening();
     }
 
-    try {
-      this.channel = await this.connection.createChannel();
-      this.channel.on('error', (err) => {
-        this.logger.error(`Channel error: ${err}`);
-        this.channel = null;
-      });
-      this.channel.on('close', () => {
-        this.logger.info(`Channel closed`);
-        this.channel = null;
-      });
-    } catch (err) {
-      this.logger.error(`Failed to create a channel: ${err}`);
-    }
-  }
-
-  private async retryConnect() {
-    let retries = 0;
-
-    while (!this.connection && retries < 5) {
-      try {
-        this.logger.info(`Attempting to connect (Attempt ${retries + 1})...`);
-        await this.connect();
-      } catch (err) {
-        this.logger.error(`Failed to connect: ${err}`);
-      }
-
-      if (!this.connection) {
-        const timeout = Math.pow(2, retries) * 1000;
-        await new Promise((resolve) => setTimeout(resolve, timeout));
-      }
-
-      retries++;
+    async onApplicationShutdown() {
+        await this.stopListening();
+        await this.closeConnection();
     }
 
-    if (!this.connection) {
-      this.logger.error(`Failed to connect after retries`);
-    }
-  }
-
-  async startListening() {
-    if (!this.channel) {
-      await this.createChannel();
-    }
-
-    if (this.channel) {
-      await this.channel.assertQueue(this.queueName, { durable: false });
-
-      this.channel.consume(this.queueName, (msg) => {
-        if (msg) {
-          this.logger.info(`Received message: ${msg.content.toString()}`);
-          this.channel?.ack(msg);
+    private async connect() {
+        try {
+            this.connection = await amqp.connect('amqp://admin:supersecretpassword@localhost:5673');
+            this.connection.on('error', (err) => {
+                this.logger.error(`Connection error: ${err}`);
+                this.connection = null;
+                this.connectionErrorCounter.inc();
+            });
+            this.connection.on('close', () => {
+                this.logger.info(`Connection closed`);
+                this.connection = null;
+            });
+            this.successfulConnectionCounter.inc();
+        } catch (err) {
+            this.logger.error(`Failed to connect: ${err}`);
+            this.connectionErrorCounter.inc();
         }
-      });
-
-      this.consuming = true;
-      this.logger.info(`Started listening to ${this.queueName}`);
     }
-  }
 
-  async stopListening() {
-    if (this.channel && this.consuming) {
-      await this.channel.cancel(this.queueName);
-      this.consuming = false;
-      this.logger.info(`Stopped listening to ${this.queueName}`);
+    private async createChannel() {
+        if (!this.connection) {
+            this.logger.error(`Cannot create a channel without a connection`);
+            return;
+        }
+
+        try {
+            this.channel = await this.connection.createChannel();
+            this.channel.on('error', (err) => {
+                this.logger.error(`Channel error: ${err}`);
+                this.channel = null;
+            });
+            this.channel.on('close', () => {
+                this.logger.info(`Channel closed`);
+                this.channel = null;
+            });
+        } catch (err) {
+            this.logger.error(`Failed to create a channel: ${err}`);
+        }
     }
-  }
 
-  private async closeConnection() {
-    if (this.connection) {
-      await this.connection.close();
-      this.logger.info(`Connection to RabbitMQ closed`);
+    private async retryConnect() {
+        let retries = 0;
+
+        while (!this.connection && retries < 5) {
+            try {
+                this.logger.info(`Attempting to connect (Attempt ${retries + 1})...`);
+                await this.connect();
+            } catch (err) {
+                this.logger.error(`Failed to connect: ${err}`);
+            }
+
+            if (!this.connection) {
+                const timeout = Math.pow(2, retries) * 1000;
+                await new Promise((resolve) => setTimeout(resolve, timeout));
+            }
+
+            retries++;
+        }
+
+        if (!this.connection) {
+            this.logger.error(`Failed to connect after retries`);
+        }
     }
-  }
 
-  getMetrics() {
-    return this.registry.metrics();
-  }
+    async startListening() {
+        if (!this.channel) {
+            await this.createChannel();
+        }
+
+        if (this.channel) {
+            await this.channel.assertQueue(this.queueName, { durable: false });
+
+            this.channel.consume(this.queueName, (msg) => {
+                if (msg) {
+
+                    const content = msg.content.toString();
+                    const json = JSON.parse(content);
+                    const { pattern, data } = json;
+                    // this.logger.info(`Received message: ${pattern} ${data}`);
+
+                    const dataJson = JSON.parse(data);
+
+                    let type = dataJson.action.value as string;
+
+                    switch (type) {
+                        case 'place-bid':
+                            this.logger.info('Place bid event received');
+                            break;
+                        case 'place-bid-return-previous-bid':
+                            this.logger.info('Place bid return previous bid event received');
+                            break;
+                        case 'start-auction':
+                            this.logger.info('Start Auction event received');
+                            break;
+                        case 'set-whitelisted':
+                            this.logger.info('Whitelist event received');
+                            break;
+                        case 'cancel-auction': 
+                            this.logger.info('Cancel Auction event received');
+                            break;
+                        default:
+                            this.logger.info('Unknown event received');
+                            break;
+                    }
+
+                    this.channel?.ack(msg);
+                }
+            });
+
+            this.consuming = true;
+            this.logger.info(`Started listening to ${this.queueName}`);
+        }
+    }
+
+    async stopListening() {
+        if (this.channel && this.consuming) {
+            await this.channel.cancel(this.queueName);
+            this.consuming = false;
+            this.logger.info(`Stopped listening to ${this.queueName}`);
+        }
+    }
+
+    private async closeConnection() {
+        if (this.connection) {
+            await this.connection.close();
+            this.logger.info(`Connection to RabbitMQ closed`);
+        }
+    }
+
+    getMetrics() {
+        return this.registry.metrics();
+    }
 }
